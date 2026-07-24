995f22423887efc5d33f79a59f9f8bc2b3f45024
Akshay Shivanne Revanna <akshay33sr@gmail.com>
2026-07-23 23:18:05 -0400

    added infrastructure and cd pipelin workflows

--- codebase/rdicidr-0.1.0/terraform/variables.tf
+++ codebase/rdicidr-0.1.0/terraform/variables.tf
@@ -1,5 +1,5 @@
 # In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "environment" {
+    type = string
+    description = "Deployment environmnt (devel or stage)"
 }
